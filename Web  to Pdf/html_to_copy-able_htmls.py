a562aba6b67c15a575d16c50aa51b800adb1d4d2
Prajwal Waykos <90028385+Praj-17@users.noreply.github.com>
2022-12-04 09:41:15 +0530

    reshaped the copyable htmls

--- Web  to Pdf/html_to_copy-able_htmls.py
+++ Web  to Pdf/html_to_copy-able_htmls.py
@@ -1,20 +1,16 @@
 from bs4 import BeautifulSoup
-import bs4
-import requests
 from selenium import webdriver
 from webdriver_manager.chrome import ChromeDriverManager
 url = 'https://www.engineersedge.com/design_guidelines.htm'
+browser = webdriver.Chrome(ChromeDriverManager().install())
 
 def get_text(url):
-
-    browser = webdriver.Chrome(ChromeDriverManager().install())
-
     browser.get(url)
     simple_soup = BeautifulSoup(browser.page_source)
 
     file_name = url.split('/')[-1]
     
     with open(f'Htmls//{file_name}.html', 'w') as f:
-        f.write('simple_soup')
+        f.write(simple_soup)
     print(f'Done with {file_name}')
     return True
